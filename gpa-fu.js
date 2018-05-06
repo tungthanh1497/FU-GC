e8e24b2e95fb9b047df4174515ef05ec8f96cf6d
zTitiK\tungthanh.1497 <tungthanh.1497@gmail.com>
2018-05-06 21:51:22 +0700

    meomeo
    add info binh.nguyenanh.5

--- gpa-fu.js
+++ gpa-fu.js
@@ -1,11 +1,12 @@
 var rows = document.getElementsByClassName("table table-hover")["0"].rows;
-var sum = 0;
-var tin = 0;
+var totalGrade = 0;
+var totalCredit = 0;
 for (i = 1; i < rows.length; i++) {
-var ispassed = rows[i].cells[8].innerText == "Passed";
-    if (rows[i].cells[7].innerText.length > 0 && ispassed && parseFloat(rows[i].cells[7].innerText)>0) {
-        sum += (parseFloat(rows[i].cells[7].innerText)*parseFloat(rows[i].cells[6].innerText));
-        tin += parseFloat(rows[i].cells[6].innerText);
+	var isPassed = rows[i].cells[8].innerText == "Passed";
+    if (rows[i].cells[7].innerText.length > 0 && isPassed && parseFloat(rows[i].cells[7].innerText)>0) {
+        totalGrade += (parseFloat(rows[i].cells[7].innerText)*parseFloat(rows[i].cells[6].innerText));
+        totalCredit += parseFloat(rows[i].cells[6].innerText);
     }
 }
-alert("GPA : " + Math.round(sum * 100 / tin) / 100);
+var gpa = Math.round(totalGrade * 100 / totalCredit) / 100;
+alert("GPA : " + gpa +"\nThank: http://fb.com/binh.nguyenanh.5" +"\n#Vedz");
